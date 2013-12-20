772cbe68edab6da4c8c9d62981bbd00653d04cd6
henryyang42 <henryyang42@gmail.com>
2013-12-20 11:28:45 +0800

    Add judge.cpp

--- Cocoon/judge/judge.cpp
+++ Cocoon/judge/judge.cpp
@@ -6,13 +6,6 @@
 #include <string>
 using namespace std;
 
-const int SUCCESS = 0;
-const int FITAL = 1;
-const int TLE = 2;
-const int MLE = 3;
-const int RE = 4;
-
-
 void parserOpt(int argc, char *argv[]);
 void showHelp();
 int main(int argc, char *argv[]) {
@@ -26,8 +19,26 @@ void parserOpt(int argc, char *argv[]) {
 	// no input data, show help message and exit
 	if (argc < 2) {
 		showHelp();
-		exit(
 	}
+	else {
+		while( (c = get_opt(argc, argv, "t:P:T:I:s")) != -1) {
+			switch (c) {
+				
+			}
+		}
+	}
+}
 
-
+void showHelp() {
+	printf("Usage: judge [Option] -P problemID -T sourceType -I *.c/*.cpp\n");
+	printf("Option:\n");
+	printf("    -t  time limit (default 1000, unit ms)\n");
+	printf("    -s  use strict judge; otherwise, use tolorent judge\n");
+	printf("Output:\n");
+	printf("    1. Compiler Error with error message\n");
+	printf("    2. Runtime Error\n");
+	printf("    3. Time Limit Exceeded\n");
+	printf("    4. Memery Limit Exceeded\n");
+	printf("    5. Wrong Answer with error message\n");
+	printf("    6. Accept\n");
 }
