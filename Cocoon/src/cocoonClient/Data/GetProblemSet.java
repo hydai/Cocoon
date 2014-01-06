cd371a13ebfe26a57aa454b39eb02ab48fc2889c
Andy <andyw330@gmail.com>
2014-01-07 02:36:00 +0800

    Get problems from file

--- Cocoon/src/cocoonClient/Data/GetProblemSet.java
+++ Cocoon/src/cocoonClient/Data/GetProblemSet.java
@@ -1,15 +1,27 @@
 package cocoonClient.Data;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class GetProblemSet {
 	private HashMap<String, Integer> map = new HashMap<String, Integer>();
+	private Scanner scanner;
 	public GetProblemSet() {
-		map.put("The A+B Problem", 1);
-		map.put("GCD", 2);
-		map.put("Palindrome", 3);
+		try {
+			scanner = new Scanner(new File("problem/problemData.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while(scanner.hasNextLine()) {
+			int pid = scanner.nextInt();
+			scanner.nextInt();
+			scanner.nextInt();
+			String problemName = scanner.nextLine();
+			map.put(problemName, pid);
+		}
 	}
 	public HashMap<String, Integer> getProblemSet(){
 		return map;
