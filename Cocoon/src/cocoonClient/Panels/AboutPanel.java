fd40e32806728eeb22fb9a9647ca432cd3f8d737
hydai <z54981220@gmail.com>
2014-01-06 22:30:36 +0800

    Add about panel

---
+++ Cocoon/src/cocoonClient/Panels/AboutPanel.java
@@ -0,0 +1,10 @@
+package cocoonClient.Panels;
+
+import cocoonClient.Data.UserInfo;
+
+public class AboutPanel extends AbstractDisplayPanel{
+	AboutPanel(){
+		super(UserInfo.getMainFrame());
+		
+	}
+}
