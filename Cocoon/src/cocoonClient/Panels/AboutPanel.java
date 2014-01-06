efadbb9cfcb19fc704672e33472a157c5002a7f1
hydai <z54981220@gmail.com>
2014-01-07 01:59:51 +0800

    Add about cocoon panel and renew transmit protocol

--- Cocoon/src/cocoonClient/Panels/AboutPanel.java
+++ Cocoon/src/cocoonClient/Panels/AboutPanel.java
@@ -1,10 +1,21 @@
 package cocoonClient.Panels;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import cocoonClient.Data.UserInfo;
 
 public class AboutPanel extends AbstractDisplayPanel{
 	public AboutPanel(){
 		super(UserInfo.getMainFrame());
+		this.setLayout(new BorderLayout());
+		// Show our Logo on north side of this panel
 		setRightPanel(new TestRightPanel());
+		this.add(new JLabel(new ImageIcon("res/About.png")), BorderLayout.NORTH);
 	}
 }
