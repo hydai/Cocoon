707c9fe3a1ece3ab4f10c91d8b8d61c4147b5e2d
hydai <z54981220@gmail.com>
2014-01-09 04:31:15 +0800

    Fixed statistics query bugs

--- Cocoon/src/cocoonServer/mysql/SQLRadarStatisticsData.java
+++ Cocoon/src/cocoonServer/mysql/SQLRadarStatisticsData.java
@@ -11,6 +11,7 @@ import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class SQLRadarStatisticsData extends SQLData{
+//	private String querySQLString = "select * from ProblemType where pid in (select pid from RadarStatistics where id = ?)";
 	private String querySQLString = "select * from RadarStatistics where id = ?";
 	private int id;
 	private int IO;
@@ -84,7 +85,7 @@ public class SQLRadarStatisticsData extends SQLData{
 			getUsernamePreparedStatement.setInt(1, id);
 			ResultSet resultSet = getUsernamePreparedStatement.executeQuery();
 			Map<Integer, Integer> solvedMap = new TreeMap<Integer, Integer>();
-			if (resultSet.next()) {
+			while (resultSet.next()) {
 				int tmpPID = resultSet.getInt("pid");
 				if (!solvedMap.containsKey(tmpPID)) {
 					solvedMap.put(tmpPID, 1);
@@ -93,11 +94,33 @@ public class SQLRadarStatisticsData extends SQLData{
 			for(Iterator<Entry<Integer, Integer>> iter = solvedMap.entrySet().iterator(); iter.hasNext();) {
 				solvedProblem.add(iter.next().getKey());
 			}
+			
 			IO = 0;
 			Math = 0;
 			Array = 0;
 			Sort = 0;
 			DataStructure = 0;
+			
+			/*
+			while (resultSet.next()) {
+				String typeString = resultSet.getString("type");
+				if (typeString.equals("IO")) {
+					IO++;
+				}
+				else if (typeString.equals("Math")) {
+					Math++;
+				}
+				else if (typeString.equals("Array")) {
+					Array++;
+				}
+				else if (typeString.equals("Sort")) {
+					Sort++;
+				}
+				else if (typeString.equals("DataStructure")) {
+					DataStructure++;
+				}
+			}
+			*/
 			SQLProblemType problemType = new SQLProblemType();
 			for(int element : solvedProblem) {
 				String typeString = problemType.getProblemType(element);
