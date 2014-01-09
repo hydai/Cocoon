38b4efd6685b826d0b28b2b6272758ef7caa80f0
hydai <z54981220@gmail.com>
2014-01-09 12:10:51 +0800

    I am god

---
+++ Cocoon/src/JSONTransmitProtocol/Main.java
@@ -0,0 +1,12 @@
+package JSONTransmitProtocol;
+
+import java.util.Collection;
+
+import org.jfree.util.HashNMap;
+import org.json.JSONArray;
+
+
+public class Main {
+	float[] fractions = { 0f, 0.5f, 1f };
+	JSONArray assArray = new JSONArray((Collection)new HashNMap());
+}
